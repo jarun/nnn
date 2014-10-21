7273c9d6819c50ab546885f7d6d0e81d03b7ca16
sin <sin@2f30.org>
2014-10-21 12:07:20 +0100

    Add flac to default associations

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 #define EMPTY "   "
 
 struct assoc assocs[] = {
-	{ "\\.(avi|mp4|mkv|mp3|ogg)$", "mplayer" },
+	{ "\\.(avi|mp4|mkv|mp3|ogg|flac)$", "mplayer" },
 	{ "\\.(png|jpg|gif)$", "feh" },
 	{ "\\.(html|svg)$", "firefox" },
 	{ "\\.pdf$", "mupdf" },
