1c23cbc2ad07ae62c47ce1b091c86dd112c469f9
lostd <lostd@2f30.org>
2014-10-23 00:12:57 +0300

    Create test files and directories

---
+++ mktest.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Create test files and directories
+
+test -e test && {
+    echo "Remove test and try again"
+    exit 1
+}
+
+mkdir test && cd test
+
+echo 'It works!' > normal.txt
+ln -s normal.txt ln-normal.txt
+ln -s normal.txt ln-normal
+mkdir normal-dir
+ln -s normal-dir ln-normal-dir
+ln -s nowhere ln-nowhere
+mkfifo mk-fifo
+touch no-access && chmod 000 no-access
+ln -s ../normal.txt normal-dir/ln-normal.txt
+ln -s ../normal.txt normal-dir/ln-normal
