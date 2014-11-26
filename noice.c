15a7a7826450237bb9b5588b5ec9c920e232e1c4
lostd <lostd@2f30.org>
2014-11-26 17:42:57 +0200

    Remember current entry accross filter changes

--- noice.c
+++ noice.c
@@ -695,6 +695,8 @@ nochange:
 			filter = tmp;
 			filter_re = re;
 			DPRINTF_S(filter);
+			/* Save current  */
+			oldpath = makepath(path, dents[cur].name);
 			goto out;
 		case SEL_NEXT:
 			if (cur < n - 1)
