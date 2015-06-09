f6fd1cc02fa33a62d1f01cbabac220c0f67b27c5
lostd <lostd@2f30.org>
2015-06-09 09:12:17 +0100

    Sort includes

--- noice.c
+++ noice.c
@@ -2,18 +2,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#include <curses.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <dirent.h>
-#include <curses.h>
 #include <libgen.h>
 #include <limits.h>
 #include <locale.h>
 #include <regex.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
