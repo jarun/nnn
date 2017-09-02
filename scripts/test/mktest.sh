335242a1eb9cf57bdc28d3f2e702246916e0fa0b
Arun Prakash Jana <engineerarun@gmail.com>
2017-09-02 18:09:47 +0530

    Make test script more aggressive

--- scripts/test/mktest.sh
+++ scripts/test/mktest.sh
@@ -7,30 +7,30 @@ test -e test && {
     exit 1
 }
 
-mkdir outdir && cd outdir
+mkdir -p outdir && cd outdir
 
 echo 'It works!' > normal.txt
 echo 'Με δουλέβει;' > 'κοινό.txt'
-ln -s normal.txt ln-normal.txt
-ln -s normal.txt ln-normal
-mkdir normal-dir
-ln -s normal-dir ln-normal-dir
-ln -s nowhere ln-nowhere
+ln -sf normal.txt ln-normal.txt
+ln -sf normal.txt ln-normal
+mkdir -p normal-dir
+ln -sf normal-dir ln-normal-dir
+ln -sf nowhere ln-nowhere
 mkfifo mk-fifo
 touch no-access && chmod 000 no-access
-mkdir no-access-dir && chmod 000 no-access-dir
-ln -s ../normal.txt normal-dir/ln-normal.txt
-ln -s ../normal.txt normal-dir/ln-normal
+mkdir -p no-access-dir && chmod 000 no-access-dir
+ln -sf ../normal.txt normal-dir/ln-normal.txt
+ln -sf ../normal.txt normal-dir/ln-normal
 echo 'int main(void) { *((char *)0) = 0; }' > ill.c
 make ill > /dev/null
 echo 'test/ill' > ill.sh
-mkdir empty-dir
-mkdir cage
+mkdir -p empty-dir
+mkdir -p cage
 echo 'chmod 000 test/cage' > cage/lock.sh
 echo 'chmod 755 test/cage' > cage-unlock.sh
-mkdir cage/lion
+mkdir -p cage/lion
 echo 'chmod 000 test/cage' > cage/lion/lock.sh
-mkdir unicode
+mkdir -p unicode
 touch 'unicode/Malgudi Days - मालगुडी डेज - E05. Swami and Friends - स्वामी और उसके दोस्त (Part 1)'
 touch 'unicode/Malgudi Days - मालगुडी डेज - E05. Swami and Friends - स्वामी और उसके दोस्त (Part 2)'
 touch 'unicode/Malgudi Days - मालगुडी डेज - E05. Swami and Friends - स्वामी और उसके दोस्त (Part 3)'
