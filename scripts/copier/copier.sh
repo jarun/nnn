4ef2230fca55015b4848ad19979223e50014e080
Arun Prakash Jana <engineerarun@gmail.com>
2018-11-30 06:19:19 +0530

    Add sample command for Termux

--- scripts/copier/copier.sh
+++ scripts/copier/copier.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 cat /path/to/.nnncp | xargs -0 | xsel -bi
+
+# Termux
+# cat /path/to/.nnncp | xargs -0 | termux-clipboard-set
+# e.g.: cat /data/data/com.termux/files/home/.nnncp | xargs -0 | termux-clipboard-set
