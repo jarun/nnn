6b7af57947d272d0e4281fde260b915092d44283
QasimK <noreply@qasimk.io>
2019-01-01 23:57:38 +0000

    Document how to install fish quitcd function
    It was not clear that the name of the file should match
    the name of the function.

--- scripts/quitcd/quitcd.fish
+++ scripts/quitcd/quitcd.fish
@@ -1,3 +1,6 @@
+# Rename this file to match the name of the function
+# e.g. ~/.config/fish/functions/n.fish
+
 export NNN_TMPFILE="/tmp/nnn"
 
 function n --description 'support nnn quit and change directory'
