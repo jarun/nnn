bc45526abd61e3be74db60e889ba397638c91ba3
Bruce Hill <bruce@bruce-hill.com>
2019-05-08 19:44:09 -0700

    Updated fallthrough comments for build

--- src/nnn.c
+++ src/nnn.c
@@ -3170,9 +3170,9 @@ nochange:
 				// Single click just selects, double click also opens
 				if (event.bstate != BUTTON1_DOUBLE_CLICKED)
 					break;
-				// fallthrough
+				// fallthrough to select the file
 			} else
-				goto nochange;
+				goto nochange; // fallthrough
 		case SEL_NAV_IN: // fallthrough
 		case SEL_GOIN:
 			/* Cannot descend in empty directories */
