f228ff11a9ed71b37059a43763df14e226e53b20
Bruce Hill <bruce@bruce-hill.com>
2019-05-09 12:33:03 -0700

    Fixing extra change again.

--- src/nnn.c
+++ src/nnn.c
@@ -2070,9 +2070,9 @@ static char *coolsize(off_t size)
 	}
 
 	if (i > 0 && i < 6)
-		snprintf(size_buf, 12, "%lu.%0*lu%c", (long)size, i, (long)rem, U[i]);
+		snprintf(size_buf, 12, "%lu.%0*lu%c", (ulong)size, i, (ulong)rem, U[i]);
 	else
-		snprintf(size_buf, 12, "%lu%c", (long)size, U[i]);
+		snprintf(size_buf, 12, "%lu%c", (ulong)size, U[i]);
 
 	return size_buf;
 }
