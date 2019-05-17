2a5b3d11d4fb180ecfc5c634d56aafbf7c11d1f9
Bruce Hill <bruce@bruce-hill.com>
2019-05-16 17:57:47 -0700

    Switched strncat -> strlcat

--- src/nnn.c
+++ src/nnn.c
@@ -1129,7 +1129,7 @@ static void rename_selection(const char *path)
 	char *name;
 
 	strncpy(fselected, g_tmpfpath, g_tmpfplen);
-	strncat(fselected, messages[STR_TMPFILE], PATH_MAX);
+	strlcat(fselected, messages[STR_TMPFILE], strlen(messages[STR_TMPFILE]));
 	strncpy(frenamed, fselected, PATH_MAX);
 
 	if ((fd1 = mkstemp(fselected)) == -1)
