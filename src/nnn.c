1027cd0cf29a418e0d01f27a9e595c3fd516ec9a
KlZX <azszwymmvqdi@yahoo.com>
2019-08-12 17:08:02 +0200

    Replaced 'sed' with 'awk'

--- src/nnn.c
+++ src/nnn.c
@@ -1299,12 +1299,7 @@ static void archive_selection(const char *cmd, const char *archive, const char *
 {
 	char *buf = (char *)malloc(CMD_LEN_MAX * sizeof(char));
 	snprintf(buf, CMD_LEN_MAX,
-#ifdef __linux__
-		"sed -ze 's|^%s/||' '%s' | xargs -0 %s %s", curpath, g_cppath, cmd, archive);
-#else
-		"cat '%s' | tr '\\0' '\n' | sed -e 's|^%s/||' | tr '\n' '\\0' | xargs -0 %s %s",
-		g_cppath, curpath, cmd, archive);
-#endif
+		"awk 'BEGIN{RS=\"\\0\"} {gsub(\"%s/\", \"\", $0); printf \"%%s\\0\", $0}' %s | xargs -0 %s %s", curpath, g_cppath, cmd, archive);
 	spawn("sh", "-c", buf, curpath, F_NORMAL);
 	free(buf);
 }
