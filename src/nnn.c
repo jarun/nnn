a9f60d0353499eac9e94c2c5ae628b5664ab679b
Arun Prakash Jana <engineerarun@gmail.com>
2019-10-02 23:57:26 +0530

    Block range selection only when dir chanegs

--- src/nnn.c
+++ src/nnn.c
@@ -153,12 +153,6 @@
 #define F_CLI     (F_NORMAL | F_MULTI)
 #define F_SILENT  (F_CLI | F_NOTRACE)
 
-/* CRC8 macros */
-#define UCHAR_BIT_WIDTH  (sizeof(unsigned char) << 3)
-#define TOPBIT (1 << (UCHAR_BIT_WIDTH - 1))
-#define POLYNOMIAL 0xD8  /* 11011 followed by 0's */
-#define CRC8_TABLE_LEN 256
-
 /* Version compare macros */
 /*
  * states: S_N: normal, S_I: comparing integral part, S_F: comparing
@@ -306,7 +300,6 @@ static ulong num_files;
 static kv bookmark[BM_MAX];
 static kv plug[PLUGIN_MAX];
 static size_t g_tmpfplen;
-static uchar g_crc;
 static uchar BLK_SHIFT = 9;
 static bool interrupted = FALSE;
 
@@ -476,46 +469,6 @@ static void move_cursor(int target, int ignore_scrolloff);
 static bool getutil(char *util);
 
 /* Functions */
-
-/*
- * CRC8 source:
- *   https://barrgroup.com/Embedded-Systems/How-To/CRC-Calculation-C-Code
- */
-static uchar crc8fast(const uchar * const message, size_t n)
-{
-	uchar data, remainder = 0;
-	size_t byte = 0;
-
-	/* CRC data */
-	static const uchar crc8table[CRC8_TABLE_LEN] __attribute__ ((aligned)) = {
-		  0,  94, 188, 226,  97,  63, 221, 131, 194, 156, 126,  32, 163, 253,  31,  65,
-		157, 195,  33, 127, 252, 162,  64,  30,  95,   1, 227, 189,  62,  96, 130, 220,
-		 35, 125, 159, 193,  66,  28, 254, 160, 225, 191,  93,   3, 128, 222,  60,  98,
-		190, 224,   2,  92, 223, 129,  99,  61, 124,  34, 192, 158,  29,  67, 161, 255,
-		 70,  24, 250, 164,  39, 121, 155, 197, 132, 218,  56, 102, 229, 187,  89,   7,
-		219, 133, 103,  57, 186, 228,   6,  88,  25,  71, 165, 251, 120,  38, 196, 154,
-		101,  59, 217, 135,   4,  90, 184, 230, 167, 249,  27,  69, 198, 152, 122,  36,
-		248, 166,  68,  26, 153, 199,  37, 123,  58, 100, 134, 216,  91,   5, 231, 185,
-		140, 210,  48, 110, 237, 179,  81,  15,  78,  16, 242, 172,  47, 113, 147, 205,
-		 17,  79, 173, 243, 112,  46, 204, 146, 211, 141, 111,  49, 178, 236,  14,  80,
-		175, 241,  19,  77, 206, 144, 114,  44, 109,  51, 209, 143,  12,  82, 176, 238,
-		 50, 108, 142, 208,  83,  13, 239, 177, 240, 174,  76,  18, 145, 207,  45, 115,
-		202, 148, 118,  40, 171, 245,  23,  73,   8,  86, 180, 234, 105,  55, 213, 139,
-		 87,   9, 235, 181,  54, 104, 138, 212, 149, 203,  41, 119, 244, 170,  72,  22,
-		233, 183,  85,  11, 136, 214,  52, 106,  43, 117, 151, 201,  74,  20, 246, 168,
-		116,  42, 200, 150,  21,  75, 169, 247, 182, 232,  10,  84, 215, 137, 107,  53,
-	};
-
-	/* Divide the message by the polynomial, a byte at a time */
-	while (byte < n) {
-		data = message[byte] ^ (remainder >> (UCHAR_BIT_WIDTH - 8));
-		remainder = crc8table[data] ^ (remainder << 8);
-		++byte;
-	}
-
-	/* The final remainder is the CRC */
-	return remainder;
-}
 
 static void sigint_handler(int sig)
 {
@@ -2489,9 +2442,6 @@ static void savecurctx(settings *curcfg, char *path, char *curname, int r /* nex
 	settings cfg = *curcfg;
 	bool selmode = cfg.selmode ? TRUE : FALSE;
 
-#ifdef DIR_LIMITED_SELECTION
-	g_crc = 0;
-#endif
 	/* Save current context */
 	xstrlcpy(g_ctx[cfg.curctx].c_name, curname, NAME_MAX + 1);
 	g_ctx[cfg.curctx].c_cfg = cfg;
@@ -3410,6 +3360,7 @@ static void browse(char *ipath)
 	char runfile[NAME_MAX + 1] __attribute__ ((aligned));
 	uchar opener_flags = (cfg.cliopener ? F_CLI : (F_NOTRACE | F_NOWAIT));
 	int r = -1, fd, presel, selstartid = 0, selendid = 0, onscreen;
+	ino_t inode = 0;
 	enum action sel;
 	bool dir_changed = FALSE, rangesel = FALSE;
 	struct stat sb;
@@ -4092,10 +4043,15 @@ nochange:
 				goto nochange;
 
 			startselection();
-			rangesel ^= 1;
+			rangesel ^= TRUE;
+
+			if (stat(path, &sb) == -1) {
+				printwarn(&presel);
+				goto nochange;
+			}
 
 			if (rangesel) { /* Range selection started */
-				g_crc = crc8fast((uchar *)dents, ndents * sizeof(struct entry));
+				inode = sb.st_ino;
 				selstartid = cur;
 				mvprintw(xlines - 1, 0, "range selection on\n");
 				xdelay();
@@ -4103,9 +4059,8 @@ nochange:
 			}
 
 #ifndef DIR_LIMITED_SELECTION
-			if (g_crc != crc8fast((uchar *)dents, ndents * sizeof(struct entry))) {
-				rangesel = 0;
-				printwait("dir/content changed, range selection off", &presel);
+			if (inode != sb.st_ino) {
+				printwait("dir changed, range selection off", &presel);
 				goto nochange;
 			}
 #endif
