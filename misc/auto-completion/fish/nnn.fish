043920df3b6789107be157f831814f99f5022e74
Anna Arad <annagram@shards.me>
2019-10-17 21:11:26 +0300

    Update fish completions with sessions and make some improvements

--- misc/auto-completion/fish/nnn.fish
+++ misc/auto-completion/fish/nnn.fish
@@ -5,17 +5,24 @@
 #   Arun Prakash Jana <engineerarun@gmail.com>
 #
 
+if test -n "$XDG_CONFIG_HOME"
+    set nnn_config $XDG_CONFIG_HOME/.config/nnn
+else
+    set nnn_config $HOME/.config/nnn
+end
+
 complete -c nnn -s a    -d 'use access time'
-complete -c nnn -s b -r -d 'bookmark key to open'
+complete -c nnn -s b -r -d 'bookmark key to open' -x -a '(echo $NNN_BMS | awk -F: -v RS=\; \'{print $1"\t"$2}\')'
 complete -c nnn -s c    -d 'cli-only opener'
 complete -c nnn -s d    -d 'start in detail mode'
+complete -c nnn -s e -r -d 'load session by name' -x -a '@\t"last session" (ls $nnn_config/sessions)'
 complete -c nnn -s f    -d 'run filter as cmd on prompt key'
 complete -c nnn -s H    -d 'show hidden files'
 complete -c nnn -s i    -d 'start in navigate-as-you-type mode'
 complete -c nnn -s K    -d 'detect key collision'
 complete -c nnn -s n    -d 'use version compare to sort files'
 complete -c nnn -s o    -d 'open files only on Enter'
-complete -c nnn -s p -r -d 'copy selection to file'
+complete -c nnn -s p -r -d 'copy selection to file' -a '-\tstdout'
 complete -c nnn -s r    -d 'show cp, mv progress (Linux-only)'
 complete -c nnn -s s    -d 'use substring match for filters'
 complete -c nnn -s S    -d 'start in disk usage analyzer mode'
