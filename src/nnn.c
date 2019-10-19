88c255e0b6e8e6e9a639c7151656a6e82a0a9088
Anna Arad <annagram@shards.me>
2019-10-19 21:03:19 +0300

    Fix bug when going to dir on non-current context from plugin

--- src/nnn.c
+++ src/nnn.c
@@ -3311,7 +3311,7 @@ static bool initialize_plugin_control() {
 	return _SUCCESS;
 }
 
-static bool run_selected_plugin(char *path, const char *file, char *newpath, char *rundir, char *runfile, char *lastname, char *lastdir)
+static bool run_selected_plugin(char **path, const char *file, char *newpath, char *rundir, char *runfile, char **lastname, char **lastdir)
 {
 	if (!g_is_plugin_control_initialized) {
 		initialize_plugin_control();
@@ -3320,22 +3320,22 @@ static bool run_selected_plugin(char *path, const char *file, char *newpath, cha
 
 	if ((cfg.runctx != cfg.curctx)
 	    /* Must be in plugin directory to select plugin */
-	    || (strcmp(path, plugindir) != 0))
+	    || (strcmp(*path, plugindir) != 0))
 		return FALSE;
 
 	int fd = open(g_pipepath, O_RDONLY | O_NONBLOCK);
 	if (fd == -1)
 		return FALSE;
 
-	mkpath(path, file, newpath);
+	mkpath(*path, file, newpath);
 	/* Copy to path so we can return back to earlier dir */
-	xstrlcpy(path, rundir, PATH_MAX);
+	xstrlcpy(*path, rundir, PATH_MAX);
 	if (runfile[0]) {
-		xstrlcpy(lastname, runfile, NAME_MAX);
-		spawn(newpath, lastname, path, path, F_NORMAL);
+		xstrlcpy(*lastname, runfile, NAME_MAX);
+		spawn(newpath, *lastname, *path, *path, F_NORMAL);
 		runfile[0] = '\0';
 	} else
-		spawn(newpath, NULL, path, path, F_NORMAL);
+		spawn(newpath, NULL, *path, *path, F_NORMAL);
 	rundir[0] = '\0';
 	cfg.runplugin = 0;
 
@@ -3348,15 +3348,15 @@ static bool run_selected_plugin(char *path, const char *file, char *newpath, cha
 		int ctx = g_buf[0] - '0';
 
 		if (ctx == 0) {
-			xstrlcpy(lastdir, path, PATH_MAX);
-			xstrlcpy(path, g_buf + 1, PATH_MAX);
+			xstrlcpy(*lastdir, *path, PATH_MAX);
+			xstrlcpy(*path, g_buf + 1, PATH_MAX);
 		} else if (ctx >= 1 && ctx <= CTX_MAX) {
 			int r = ctx - 1;
 			g_ctx[r].c_cfg.ctxactive = 0;
 			savecurctx(&cfg, g_buf + 1, dents[cur].name, r);
-			path = g_ctx[r].c_path;
-			lastdir = g_ctx[r].c_last;
-			lastname = g_ctx[r].c_name;
+			*path = g_ctx[r].c_path;
+			*lastdir = g_ctx[r].c_last;
+			*lastname = g_ctx[r].c_name;
 		}
 	}
 
@@ -4148,7 +4148,7 @@ nochange:
 
 				/* Handle plugin selection mode */
 				if (cfg.runplugin) {
-					if (!run_selected_plugin(path, dents[cur].name, newpath, rundir, runfile, lastname, lastdir))
+					if (!run_selected_plugin(&path, dents[cur].name, newpath, rundir, runfile, &lastname, &lastdir))
 						continue;
 
 					setdirwatch();
