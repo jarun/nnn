1a02f910a051cf10352997049bd3543f2fd7b6bf
Vidar Holen <spam@vidarholen.net>
2019-11-25 20:51:03 -0800

    Misc shell script improvements
    See discussion on:
    https://github.com/jarun/nnn/commit/1cca9e4b72b106374f203890b266f18609deefe3

--- misc/quitcd/quitcd.zsh
+++ misc/quitcd/quitcd.zsh
@@ -1,14 +1,14 @@
 n ()
 {
     # Block nesting of nnn in subshells
-    if [ "$((NNNLVL + 0))" -ge 1 ]; then
+    if [ "${NNNLVL:-0}" -ge 1 ]; then
         echo "nnn is already running"
         return
     fi
 
     # The default behaviour is to cd on quit (nnn checks if NNN_TMPFILE is set)
     # To cd on quit only on ^G, export NNN_TMPFILE after the call to nnn
-    export NNN_TMPFILE=${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
 
     # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
     # stty start undef
