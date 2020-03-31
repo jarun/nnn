47ec05565120a9637ec6d94fd1889b82d9fd93a0
Arun Prakash Jana <engineerarun@gmail.com>
2020-04-01 02:26:32 +0530

    Fix #499: broken readline prompt

--- src/nnn.c
+++ src/nnn.c
@@ -1710,9 +1710,9 @@ static int spawn(char *file, char *arg1, char *arg2, const char *dir, uchar flag
 		if (flag & F_NORMAL) {
 			if (flag & F_CONFIRM) {
 				printf("%s", messages[MSG_CONTINUE]);
+				fflush(stdout);
 				while (getchar() != '\n');
 			}
-
 			refresh();
 		}
 
