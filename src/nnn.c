5f3faf613a920f5a084425affd5449773ac26479
Arun Prakash Jana <engineerarun@gmail.com>
2020-05-10 22:50:29 +0530

    Do not leak wfd

--- src/nnn.c
+++ src/nnn.c
@@ -4305,7 +4305,7 @@ static bool run_selected_plugin(char **path, const char *file, char *runfile, ch
 	exitcurses();
 
 	if (fork() == 0) { // In child
-		int wfd = open(g_pipepath, O_WRONLY);
+		int wfd = open(g_pipepath, O_WRONLY | O_CLOEXEC);
 
 		if (wfd == -1)
 			_exit(EXIT_FAILURE);
