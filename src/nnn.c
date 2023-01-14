8a1e32d9eb191db2f1b75fda6f15bf6625ecd3fa
KlzXS <klzx+github@klzx.cf>
2023-01-14 23:18:22 +0100

    Make CI happy

--- src/nnn.c
+++ src/nnn.c
@@ -4511,7 +4511,9 @@ static bool get_output(char *file, char *arg1, char *arg2, int fdout, bool page)
 
 		unlink(g_tmpfpath);
 		return TRUE;
-	} else if (have_file)
+	}
+
+	if (have_file)
 		// Case 3
 		return TRUE;
 
